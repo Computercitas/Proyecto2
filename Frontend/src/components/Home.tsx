03165b3a4dc29e3034e541f037bfc995ad2bad0d
NoemiHuarino-utec <91573449+NoemiHuarino-utec@users.noreply.github.com>
2024-11-11 00:17:41 -0500

    home actual

--- Frontend/src/components/Home.tsx
+++ Frontend/src/components/Home.tsx
@@ -1,18 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";  // Para usar rutas de navegación
-
-const Home: React.FC = () => {
+import { Link } from "react-router-dom";
+import { FaGithub } from "react-icons/fa"; // Ícono de GitHub
+import '../App.css';
+const Home = () => {
   return (
     <div className="home-container">
-      <h1>¡Bienvenidos!</h1>
-      <p>Esta es una página para probar el índice invertido con un dataset de Spotify. Puedes realizar consultas tipo SQL y obtener resultados de forma eficiente.</p>
-      <div className="button-container">
-        <Link to="/consulta">
-          <button className="consulta-button">Consulta!</button>
-        </Link>
-        <a href="https://github.com/Dateadores/Proyecto2" target="_blank" rel="noopener noreferrer">
-          <button className="github-button">Github</button>
-        </a>
+      <div className="content-overlay">
+        <h1>Bienvenidos!</h1>
+        <p>
+          Esta página permite realizar consultas sobre un índice invertido
+          usando un dataset de Spotify. Elige tu método de indexación y
+          realiza consultas de texto libre.
+        </p>
+        <div className="button-container">
+          <Link to="/consulta">
+            <button className="consulta-button">Consulta!</button>
+          </Link>
+          <a href="https://github.com/Dateadores/Proyecto2" target="_blank" rel="noopener noreferrer">
+            <button className="github-button">
+              <FaGithub size={20} /> GitHub
+            </button>
+          </a>
+        </div>
       </div>
     </div>
   );
